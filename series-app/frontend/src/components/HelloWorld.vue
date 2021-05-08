de7832c5bffbca5f9bd5c48981cdd71a1a5d831a
devrangel <matheusrangel.dev@gmail.com>
2021-05-08 11:51:45 -0300

    [series-app] Project created

---
+++ series-app/frontend/src/components/HelloWorld.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-container>
+    <h1>Series App</h1>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    name: 'HelloWorld',
+
+    data: () => ({
+      
+    }),
+  }
+</script>
