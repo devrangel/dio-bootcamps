4b529f1cfe215e51bb993cf038cc2e9ce78c946c
devrangel <matheusrangel.dev@gmail.com>
2021-05-11 17:23:04 -0300

    [shop-app] Removed unused classes

--- shop-app/backend/backend/Controllers/CategoryController.cs
+++ shop-app/backend/backend/Controllers/CategoryController.cs
@@ -1,8 +1,6 @@
 using backend.Models;
 using backend.Services;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
