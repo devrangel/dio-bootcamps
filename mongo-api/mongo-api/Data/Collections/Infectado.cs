80019fb82086afedf310c2a9a85d654ca37d81d2
devrangel <matheusrangel.dev@gmail.com>
2021-05-13 11:37:31 -0300

    [mongo-api] Initial commit

---
+++ mongo-api/mongo-api/Data/Collections/Infectado.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using MongoDB.Driver.GeoJsonObjectModel;
+
+namespace mongo_api.Data.Collections
+{
+    public class Infectado
+    {
+
+        public DateTime DataNascimento { get; set; }
+        public string Sexo { get; set; }
+        public GeoJson2DGeographicCoordinates Localizacao { get; set; }
+
+        public Infectado(DateTime dataNascimento, string sexo, double latitude, double longitude)
+        {
+            DataNascimento = dataNascimento;
+            Sexo = sexo;
+            Localizacao = new GeoJson2DGeographicCoordinates(longitude, latitude);
+        }
+    }
+}
